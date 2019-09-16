3de2312f0be95a666b190ef6e7ea58e2328e39a0
KeystoneDefiant <chris@port14.com>
2019-09-16 15:26:40 -0400

    Added a little extra help on the landing view

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,11 +1,25 @@
 <template>
   <section>
-    Hi
+    <v-img src="@/assets/img/elwinator.png"></v-img>
+
+    <v-card
+      :elevation="6"
+      raised>
+        <v-card-title>Sup Bruh.</v-card-title>
+        <v-card-text>I'm lifting all this heavy text to <span class="font-weight-black">{{textLocation}}</span></v-card-text>
+    </v-card>
+
   </section>
 </template>
 
 <script>
+const os = require('os');
 export default {
+  data: function() {
+		return{
+			textLocation: os.homedir()+"\\elwinator\\"
+		}
+	},
 };
 </script>
 
